aac6983f086cc596d481407ce2b0029006b30ad0
Kresna Satya <kresnasatya72@gmail.com>
2024-10-16 09:47:20 +0800

    fix: improve enhancements and SPRecommendation components

--- js/components/omniunit/Enhancements.js
+++ js/components/omniunit/Enhancements.js
@@ -5,9 +5,9 @@ export default function (enhancements) {
     fragment.appendChild(document.createRange().createContextualFragment(
       `<li class="my-4 py-2 border-b">
         <div class="flex justify-between items-center">
-          <span class="w-9/12 inline-block leading-loose">${enhancement.option}</span> <button class="border rounded-full border-yellow-600 bg-blue-600 text-white w-10 h-10">${enhancement.cost}</button>
+          <span class="w-9/12 inline-block leading-loose">${enhancement.option}</span><span class="border rounded-full border-yellow-600 bg-blue-600 text-white content-center text-center w-10 h-10">${enhancement.cost}</span>
         </div>
-        ${ enhancement.detail_option !== "" ? `<span class="text-sm italic">${enhancement.detail_option}</span>` : ''}
+        ${ enhancement.detail_option ? `<span class="text-sm italic">${enhancement.detail_option}</span>` : ''}
       </li>`
     ));
   }
